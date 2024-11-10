16ef50f9e9441049cf8b5dd5e9bb56f692f3fab3
jo3ypx <jpedroramos96@gmail.com>
2024-11-10 19:17:29 +0000

    Modify files

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,3 +11,8 @@ class CalcTests (SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers together."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
